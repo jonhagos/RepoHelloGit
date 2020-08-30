ecafa5678fa2d6497726fc32baa81d46ae11f704
jonhagos <jon4geo@gmail.com>
2020-08-30 11:12:08 +0200

    Initial commit

---
+++ src/HelloGit.java
@@ -0,0 +1,9 @@
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Git kan telle til 10");
+        for(int i = 1;i <= 10; i++){
+            System.out.print(i + ",");
+        }
+    }
+}
+
